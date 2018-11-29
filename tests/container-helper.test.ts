8eacc9570f866cf9de9eda85f702c0c2ac837310
Jaydee <jindongp@mhand.net>
2018-11-29 17:31:20 +0900

    added container-helper test cases.

--- tests/container-helper.test.ts
+++ tests/container-helper.test.ts
@@ -1,4 +1,4 @@
-import { crossReference } from '../src/lib/container-helpers';
+import { crossReference, subsets, selfReference } from '../src/lib/container-helpers';
 import { Candidate } from '../src/lib/types';
 
 describe('crossReference() tests', () =>{
@@ -28,5 +28,33 @@ describe('crossReference() tests', () =>{
       instantiator: async () => null
     };
     expect(crossReference(a, b)).toBe(false);
+  });
+});
+
+describe('subsets() tests', () => {
+  test('each length of results of subsets() function must be same as paremter n', () => {
+    const arr = ['a', 'b', 'c', 'd'];
+    subsets(arr, 3).map((elem) => expect(elem.length).toBe(3));
+    subsets(arr, 2).map((elem) => expect(elem.length).toBe(2));
+  });
+});
+
+describe('selfReference() tests', () => {
+  test('if a candidate has a self-reference, the function must returns true', () => {
+    const a: Candidate = {
+      key: 'A',
+      deps: ['A', 'B'],
+      instantiator: async () => null
+    };
+    expect(selfReference(a)).toBe(true);
+  });
+
+  test('if a candidate has no self-reference, the function must returns false', () => {
+    const a: Candidate = {
+      key: 'A',
+      deps: ['B', 'C'],
+      instantiator: async () => null
+    };
+    expect(selfReference(a)).toBe(false);
   });
 });
