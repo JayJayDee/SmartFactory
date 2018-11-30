37c1b646f64fa0ffc1930dafd6129dd9517cd5ae
Jaydee <jindongp@mhand.net>
2018-11-30 10:40:48 +0900

    added more test cases.

--- tests/container-helper.test.ts
+++ tests/container-helper.test.ts
@@ -1,7 +1,8 @@
-import { crossReference, subsets, selfReference } from '../src/lib/container-helpers';
+import { crossReference, subsets, selfReference, duplicateKeys, checkCyclicReference, checkSelfReference, checkKeyDuplicates } from '../src/lib/container-helpers';
 import { Candidate } from '../src/lib/types';
+import { CyclicReferenceError, SelfReferenceError, DuplicateModuleKeyError } from '../src/lib/errors';
 
-describe('crossReference() tests', () =>{
+describe('crossReference tests', () => {
   test('if there is cross-reference between two candidates, the function must be returns true', () => {
     const a: Candidate = {
       key: 'A',
@@ -29,9 +30,20 @@ describe('crossReference() tests', () =>{
     };
     expect(crossReference(a, b)).toBe(false);
   });
+
+  test('if there is cross-reference, checkCrossReference() must be throw error', () => {
+    const arr: Candidate[] = [
+      { key: 'A', deps: ['B'], instantiator: async () => null },
+      { key: 'B', deps: ['A'], instantiator: async () => null },
+      { key: 'C', deps: ['A'], instantiator: async () => null },
+    ];
+    expect(() => {
+      checkCyclicReference(arr);
+    }).toThrow(CyclicReferenceError);
+  });
 });
 
-describe('subsets() tests', () => {
+describe('subsets tests', () => {
   test('each length of results of subsets() function must be same as paremter n', () => {
     const arr = ['a', 'b', 'c', 'd'];
     subsets(arr, 3).map((elem) => expect(elem.length).toBe(3));
@@ -39,7 +51,7 @@ describe('subsets() tests', () => {
   });
 });
 
-describe('selfReference() tests', () => {
+describe('selfReference tests', () => {
   test('if a candidate has a self-reference, the function must returns true', () => {
     const a: Candidate = {
       key: 'A',
@@ -57,4 +69,39 @@ describe('selfReference() tests', () => {
     };
     expect(selfReference(a)).toBe(false);
   });
+
+  test('if there is self-reference, checkSelfReference() must be throw error', () => {
+    const arr: Candidate[] = [
+      { key: 'A', deps: ['A'], instantiator: async () => null },
+      { key: 'C', deps: ['A'], instantiator: async () => null },
+    ];
+    expect(() => {
+      checkSelfReference(arr);
+    }).toThrow(SelfReferenceError);
+  });
 });
+
+describe('duplicatedKeys tests', () => {
+  test('if duplicated keys in cadidates, the function must returns duplicated only', () => {
+    const candidates: Candidate[] = [
+      { key: 'A', deps: [], instantiator: async () => null },
+      { key: 'B', deps: [], instantiator: async () => null },
+      { key: 'A', deps: [], instantiator: async () => null }
+    ];
+    const duplicates = duplicateKeys(candidates);
+    expect(duplicates.length).toBe(1);
+    expect(duplicates[0]).toEqual('A');
+  });
+
+  test('if duplicated keys in cadidates, checkKeyDuplicates() must be throw error', () => {
+    const candidates: Candidate[] = [
+      { key: 'A', deps: [], instantiator: async () => null },
+      { key: 'B', deps: [], instantiator: async () => null },
+      { key: 'A', deps: [], instantiator: async () => null }
+    ];
+    expect(() => {
+      checkKeyDuplicates(candidates);
+    }).toThrow(DuplicateModuleKeyError);
+  });
+});
+
