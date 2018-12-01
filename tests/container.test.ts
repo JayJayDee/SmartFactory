a93ee60f5918ef97f148bc1934b0121736af7616
JayJayDee <jindongp@gmail.com>
2018-12-01 18:14:08 +0900

    fixed container ready() misjudgement for CyclicReferenceError added more test cases.

--- tests/container.test.ts
+++ tests/container.test.ts
@@ -1,5 +1,6 @@
 import { Candidate, ContainerLogger } from "../src/lib/types";
 import { injectableFunc, readyFunc } from '../src/lib/container';
+import { CyclicReferenceError, DuplicateModuleKeyError, SelfReferenceError } from "../src/lib/errors";
 
 describe('injectableFunc tests', () => {
   test('after injectable() function executed, elements must be added to candidiates array', () => {
@@ -17,15 +18,44 @@ describe('readyFunc tests', () => {
     debug: (payload: any) => null
   };
 
-  test('after ready(), candidate key must be in instance map.', () => {
-    const instanceMap = new Map<string, any>();
+  test('if ready() function works normally, instanceMap must have one candidate', () => {
     const candidates: Candidate[] = [
-      { key: 'test', deps: [], instantiator: async () => 1 }
+      { key: 'test', deps: [], instantiator: async () => 3 }
     ];
-    const ready = readyFunc({}, dummyLogger, candidates, instanceMap);
+    const instanceMap = new Map<string, any>();
+    const ready = readyFunc(null, dummyLogger, candidates, instanceMap);
     ready().then(() => {
-      expect(instanceMap.get('test')).not.toBeNull();
-      expect(instanceMap.get('test')).toBe(1);
+      expect(instanceMap.size).toBe(1);
+      expect(instanceMap.get('test')).toBe(3);
     });
+  });
+
+  test('if there is cyclic-reference, must be throw error when ready()', () => {
+    const candidates: Candidate[] = [
+      { key: 'A', deps: ['B'], instantiator: async () => 1 },
+      { key: 'B', deps: ['A'], instantiator: async () => 2 }
+    ];
+    const instanceMap = new Map<string, any>();
+    const ready = readyFunc(null, dummyLogger, candidates, instanceMap);
+    expect(ready()).rejects.toBeInstanceOf(CyclicReferenceError);
+  });
+
+  test('if there are duplicated candidates, must be throw error when ready()', () => {
+    const candidates: Candidate[] = [
+      { key: 'A', deps: [], instantiator: async () => 1 },
+      { key: 'A', deps: [], instantiator: async () => 2 }
+    ];
+    const instanceMap = new Map<string, any>();
+    const ready = readyFunc(null, dummyLogger, candidates, instanceMap);
+    expect(ready()).rejects.toBeInstanceOf(DuplicateModuleKeyError);
+  });
+
+  test('if there is self-reference, must be throw error when ready()', () => {
+    const candidates: Candidate[] = [
+      { key: 'A', deps: ['A'], instantiator: async () => 1 }
+    ];
+    const instanceMap = new Map<string, any>();
+    const ready = readyFunc(null, dummyLogger, candidates, instanceMap);
+    expect(ready()).rejects.toBeInstanceOf(SelfReferenceError);
   });
 });
